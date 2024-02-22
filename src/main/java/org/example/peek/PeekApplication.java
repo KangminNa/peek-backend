f8526f85a58b420c3a1ba93aef9955d9220bc0e2
나강민 <139530542+KangminNa@users.noreply.github.com>
2024-02-22 17:38:26 +0900

    Initial commit

---
+++ src/main/java/org/example/peek/PeekApplication.java
@@ -0,0 +1,13 @@
+package org.example.peek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeekApplication.class, args);
+    }
+
+}
